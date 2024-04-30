fa64add0c18b1314ca33c23af588646828f7a76c
rakibhow203 <rakibhossainhow65@gmail.com>
2024-04-30 22:44:42 +0600

    single product update and delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -35,10 +35,17 @@ async function run() {
     // await client.connect();
 
 const database = client.db("paintStore");
-        const productCollection = database.collection("addProduct");
+    const productCollection = database.collection("addProduct");
     
+const subCategoryCollection = database.collection("subcategory");
     
-
+  //  new category
+    app.get("/subCategory", async (req, res) => {
+      const cursor = subCategoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
@@ -48,21 +55,67 @@ const database = client.db("paintStore");
       console.log(result);
       res.send(result);
 })
-    app.post("/TopCategory", async (req, res) => {
-      console.log(req.body);
-      const result = await productCollection.insertOne(req.body);
-      console.log(result);
+
+    // works 3
+    
+    app.get("/getProduct/:id", async (req, res) => {
+      const product = req.params.id
+      const query = { _id:new ObjectId(product)};
+      const result = await productCollection.findOne(query);
       res.send(result);
-})
+    })
 
-
+    app.get("/topBrand", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
     
     app.get("/myArtAndCraft/:email", async (req, res) => {
-      console.log(req.params.email);
+      // console.log(req.params.email);
       const result = await productCollection.find({ email: req.params.email }).toArray();
       res.send(result);
     })
     
+
+    // update
+       app.get("/singleProduct/:id", async (req, res) => {
+      console.log(req.params.id);
+         const result = await productCollection.findOne({
+           _id: new ObjectId(req.params.id),
+         });
+console.log(result);
+      res.send(result);
+    })
+
+
+    app.put('/updateProduct/:id', async (req, res) => {
+  console.log(req.params.id);
+      const id = req.params.id;
+      const filter = { _id: new ObjectId (req.params.id) };
+ 
+      const data = {
+        $set: {
+          productName: req.body.productName,
+           category:req.body.category,
+           price:req.body.price,
+           time:req.body.time,
+          image:req.body.image,
+          customization:req.body.customization,
+          rating:req.body.rating,
+          stock:req.body.stock,
+          description:req.body.description,
+        },
+      };
+      const result = await addCollection.updateOne(filter, data, options);
+      res.send(result);
+      console.log(result);
+      res.send(result)
+    });
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
